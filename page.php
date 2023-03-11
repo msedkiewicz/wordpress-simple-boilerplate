55a892ffdde45a62795599b51ba0da91a4a0ac96
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 21:54:09 +0100

    Add header and footer

--- page.php
+++ page.php
@@ -1,6 +1,8 @@
 <?php
+get_header();
     while(have_posts()) {
         the_post(); ?>
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
-<?php } ?>
+<?php }
+get_footer(); ?>
--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php
+get_header();
 
 while(have_posts()) {
     the_post(); ?>
@@ -6,5 +7,5 @@ while(have_posts()) {
     <?php the_content(); ?>
     <?php
 }
-
+get_footer();
 ?>
