c871142e80373ff571ac359df2a8389033bab6c1
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 21:16:15 +0100

    Fix loop numeration

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-    $names = array('Jane', 'John', 'Tom', 'Jerry');
+    $names = array('Jane', 'John', 'Tom', 'Jerry', 'Anna');
 
     $count = 0;
 
-    while($count < 4){
+    while($count < count($names)){
     echo "<p>Hi, my name is $names[$count]</p>";
     $count++;
     }
