579e406f538a59144a74af12b98eb27b1a319910
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-11 10:37:17 +0100

    First coding approach

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
-<h1>Hello World!</h1>
-<?php ?>
+<?php
+
+function greet($name, $color){
+    echo "<p>Hi. My $name is My favourite color is $color.</p>";
+}
+
+greet('John', 'red');
+greet('Jane', 'blue');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<h2><?php bloginfo('description'); ?></h2>
