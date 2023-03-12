b5d5e68ebb4d7c3b1c0f9531083613fddf10223f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-12 20:28:31 +0100

    Add children page menu rendering

--- page.php
+++ page.php
@@ -25,15 +25,21 @@ while(have_posts()) {
         <?php }
     ?>
 
-        <!--
         <div class="page-links">
           <h2 class="page-links__title"><a href="#">About Us</a></h2>
           <ul class="min-list">
-            <li class="current_page_item"><a href="#">Our History</a></li>
-            <li><a href="#">Our Goals</a></li>
+              <?php
+              if ($parentPage) {
+                  $isChildrenPage = $parentPage;
+              } else {
+                  $isChildrenPage = get_the_ID();
+              }
+              wp_list_pages(array(
+                  'title_li' => NULL,
+                  'child_of' => $isChildrenPage,
+              )); ?>
           </ul>
         </div>
-        -->
 
         <div class="generic-content">
             <?php the_content(); ?>
