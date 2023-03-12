eff160f873ced558410e04f9b4555b1b5e051efa
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-12 18:05:20 +0100

    Add dynamic adding of parent page

--- page.php
+++ page.php
@@ -17,9 +17,10 @@ while(have_posts()) {
 
     <div class="container container--narrow page-section">
     <?php
-        if(wp_get_post_parent_id(get_the_ID())) { ?>
+    $parentPage = wp_get_post_parent_id(get_the_ID());
+        if($parentPage) { ?>
             <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title(); ?></span></p>
+            <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parentPage); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parentPage); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
             </div>
         <?php }
     ?>
