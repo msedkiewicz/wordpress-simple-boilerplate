47e0cbee98025c9e76f59097c3f144fba272126e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-12 13:40:54 +0100

    Add footer

--- footer.php
+++ footer.php
@@ -1,5 +1,63 @@
-<p>Greetings from footer.php</p>
+<footer class="site-footer">
+    <div class="site-footer__inner container container--narrow">
+        <div class="group">
+            <div class="site-footer__col-one">
+                <h1 class="school-logo-text school-logo-text--alt-color">
+                    <a href="#"><strong>Jeden</strong> krok</a>
+                </h1>
+                <p>Stworzone przez:<a class="site-footer__link" href="https://lenasedkiewicz.com/"> &copy; Lena Sędkiewicz</a></p>
+            </div>
 
-<?php wp_footer(); ?> // adding admin bar
+            <div class="site-footer__col-two-three-group">
+                <div class="site-footer__col-two">
+                    <h3 class="headline headline--small">Dowiedz się więcej</h3>
+                    <nav class="nav-list">
+                        <ul>
+                            <li><a href="#">O mnie</a></li>
+                            <li><a href="#">Narzędzia</a></li>
+                            <li><a href="#">Wydarzenia</a></li>
+                            <li><a href="#">Mroczne Krainy</a></li>
+                        </ul>
+                    </nav>
+                </div>
+
+                <div class="site-footer__col-three">
+                    <h3 class="headline headline--small">O mnie</h3>
+                    <nav class="nav-list">
+                        <ul>
+                            <li><a href="https://lenasedkiewicz.com/">Strona Lena Sędkiewicz</a></li>
+                            <li><a href="#">Blog</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="site-footer__col-four">
+                <h3 class="headline headline--small">Kontakt</h3>
+                <nav>
+                    <ul class="min-list social-icons-list group">
+                        <li>
+                            <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        </li>
+                        <li>
+                            <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
 </body>
 </html>
