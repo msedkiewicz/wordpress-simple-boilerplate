f5b59d326966fcfcecda2c98db8d9ac0eb8487e3
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-15 15:12:10 +0100

    Change events permalink

--- single-event.php
+++ single-event.php
@@ -14,7 +14,7 @@ while(have_posts()) {
     </div>
     <div class="container container--narrow page-section">
         <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo site_url('/events/'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Events&nbsp;&nbsp;</a>&nbsp;&nbsp;<?php the_title(); ?></span>&nbsp;&nbsp;</p>
+            <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Wydarzenia&nbsp;&nbsp;</a>&nbsp;&nbsp;<?php the_title(); ?></span>&nbsp;&nbsp;</p>
         </div>
         <div>
             <?php the_post_thumbnail(); ?>
