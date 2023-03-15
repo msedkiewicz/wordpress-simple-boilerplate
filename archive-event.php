4a3d1e10973e7376919dba34e80392c01fc3bbae
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-15 15:07:25 +0100

    Change events archive display

--- archive-event.php
+++ archive-event.php
@@ -15,14 +15,14 @@ get_header();
         <?php
         while(have_posts()) {
             the_post(); ?>
-            <div class="post-item">
-                <h2 class="headline headline--medium headline--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                <div class="metabox">
-                    <p>Autor: <?php the_author_posts_link(); ?>, opublikowane: <?php the_time('l, j F Y'); ?>, kategoria: <?php echo get_the_category_list(', '); ?></p>
-                </div>
-                <div class="generic-content">
-                    <?php the_excerpt(); ?>
-                    <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Więcej &raquo</a></p>
+            <div class="event-summary">
+                <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                    <span class="event-summary__month">Mar</span>
+                    <span class="event-summary__day">25</span>
+                </a>
+                <div class="event-summary__content">
+                    <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                    <p><?php echo wp_trim_words(get_the_content(), 15); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Więcej</a></p>
                 </div>
             </div>
             <?php
