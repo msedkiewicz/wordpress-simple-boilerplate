afd6451f5c974d8d8116d40ced8432b502e0b414
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-22 16:06:40 +0100

    Change home page header section

--- front-page.php
+++ front-page.php
@@ -3,10 +3,10 @@
 <div class="page-banner">
     <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/jeden-krok-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
-        <h1 class="headline headline--large">Welcome!</h1>
-        <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
-        <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <h1 class="headline headline--large">Jeden krok</h1>
+        <h2 class="headline headline--medium">Zrób dziś jeden krok w stronę zmiany!</h2>
+        <h3 class="headline headline--small">Zobacz, jaki <strong>warsztat</strong> może być dla Ciebie pomocny?</h3>
+        <a href="<?php echo get_post_type_archive_link('workshop')?>" class="btn btn--large btn--blue">Znajdź warsztat dla siebie!</a>
     </div>
 </div>
 
