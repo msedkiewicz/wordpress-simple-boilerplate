22a9b8f212e1fe6620c8547c8d0200352837846f
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-29 20:02:05 +0200

    Add page header function logic

--- functions.php
+++ functions.php
@@ -54,15 +54,27 @@ function jk_adjust_queries($query) {
 add_action('pre_get_posts', 'jk_adjust_queries');
 
 /* Reusable function for page banner */
-function pageBanner() {
-
+function pageBanner($args) {
+    if (!$args['title']) {
+        $args['title'] = get_the_title();
+    }
+    if (!$args['subtitle']) {
+        $args['subtitle'] = get_field('page_banner');
+    }
+    if (!$args['photo']) {
+        if (get_field('page_background_img')) {
+            $args['photo'] = get_field('page_background_img')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
     ?>
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php $bannerImg = get_field('page_background_img'); echo $bannerImg['sizes']['pageBanner'] ?>);"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
         <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title"><?php the_title(); ?></h1>
+            <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
             <div class="page-banner__intro">
-                <p><?php the_field('page_banner') ?></p>
+                <p><?php echo $args['subtitle'] ?></p>
             </div>
         </div>
     </div>
