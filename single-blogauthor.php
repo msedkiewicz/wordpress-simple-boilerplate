5489d2a91835095b47851d262036552fa1834048
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-29 14:45:38 +0200

    Add custom thumbnail to author page

--- single-blogauthor.php
+++ single-blogauthor.php
@@ -15,7 +15,7 @@ while(have_posts()) {
     <div class="container container--narrow page-section">
         <div class="generic-content">
             <div class="row group">
-                <div class="one-third"><?php the_post_thumbnail(); ?></div>
+                <div class="one-third"><?php the_post_thumbnail('authorPortrait'); ?></div>
                 <div class="two-thirds"><?php the_content(); ?></div>
             </div>
         </div>
