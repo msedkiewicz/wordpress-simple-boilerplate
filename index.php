1e18a779ff771d3879ec90502b0655da331d9a5b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-29 20:33:17 +0200

    Update main blog page header

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
 <?php
 get_header();
+pageBanner(array(
+    'title' => 'Blog',
+    'subtitle' => 'Najnowsze wpisy'
+));
 ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">Blog</h1>
-            <div class="page-banner__intro">
-                <p>Najnowsze wpisy</p>
-            </div>
-        </div>
-    </div>
 
     <div class="container container--narrow page-section">
     <?php
