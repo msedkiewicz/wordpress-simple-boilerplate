7f376476681208b25b99f34d6600ff2f6e4d402b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-29 20:35:18 +0200

    Update events archive header

--- page-archiwum-wydarzen.php
+++ page-archiwum-wydarzen.php
@@ -1,15 +1,10 @@
 <?php
 get_header();
+pageBanner(array(
+    'title' => 'Archiwum wydarzeń',
+    'subtitle' => 'Zobacz, co wydarzyło się w ostatnim roku!'
+));
 ?>
-    <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-        <div class="page-banner__content container container--narrow">
-            <h1 class="page-banner__title">Archiwum wydarzeń</h1>
-            <div class="page-banner__intro">
-                <p>Zobacz, co wydarzyło się w ostatnim roku!</p>
-            </div>
-        </div>
-    </div>
 
     <div class="container container--narrow page-section">
         <?php
