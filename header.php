6a7f51ae047bd5d4d3e5ae28423a61ce4f70633e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-06-06 22:24:18 +0200

    Remove unnecessary code from header

--- header.php
+++ header.php
@@ -1,27 +1,25 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> >
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>"
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?> >
-<header class="site-header">
-    <div class="container">
-        <h1 class="school-logo-text float-left">
-            <a href="<?php echo site_url() ?>"><strong>Jeden</strong> krok</a>
+<header class="">
+    <div class="">
+        <h1 class="">
+            <a href="<?php echo site_url() ?>"><!--add site name and desc rendering --></a>
         </h1>
-        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
-        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
-        <div class="site-header__menu group">
-            <nav class="main-navigation">
+        <span class=""><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="" aria-hidden="true"></i>
+        <div class="">
+            <nav class="">
             <?php wp_nav_menu(array(
                 'theme_location' => 'headerMenu',
             )); ?>
             </nav>
             <div class="site-header__util">
-                <a href="#" class="btn btn--small btn--orange float-left push-right">Logowanie</a>
-                <a href="#" class="btn btn--small btn--dark-orange float-left">Rejestracja</a>
                 <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
             </div>
         </div>
