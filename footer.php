f44b4f6334265dd1226f8613b80b0006b25045db
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-06-06 22:35:54 +0200

    Remoe unnecessary footer code

--- footer.php
+++ footer.php
@@ -1,26 +1,26 @@
-<footer class="site-footer">
-    <div class="site-footer__inner container container--narrow">
-        <div class="group">
-            <div class="site-footer__col-one">
-                <h1 class="school-logo-text school-logo-text--alt-color">
+<footer class="">
+    <div class="">
+        <div class="">
+            <div class="">
+                <h1 class="">
                     <a href="<?php echo site_url() ?>"><strong>Jeden</strong> krok</a>
                 </h1>
-                <p>Stworzone przez:<a class="site-footer__link" href="https://lenasedkiewicz.com/"> &copy; Lena Sędkiewicz</a></p>
+                <p>Stworzone przez:<a class="" href=""> &copy; </a></p>
             </div>
 
-            <div class="site-footer__col-two-three-group">
-                <div class="site-footer__col-two">
-                    <h3 class="headline headline--small">Dowiedz się więcej</h3>
-                    <nav class="nav-list">
+            <div class="">
+                <div class="">
+                    <h3 class="">Lorem ipsum</h3>
+                    <nav class="">
                         <?php wp_nav_menu(array(
                             'theme_location' => 'footerMenuOne',
                         )); ?>
                     </nav>
                 </div>
 
-                <div class="site-footer__col-three">
-                    <h3 class="headline headline--small">O mnie</h3>
-                    <nav class="nav-list">
+                <div class="">
+                    <h3 class="">Lorem ipsum</h3>
+                    <nav class="">
                         <?php wp_nav_menu(array(
                             'theme_location' => 'footerMenuTwo',
                         )); ?>
@@ -28,24 +28,24 @@
                 </div>
             </div>
 
-            <div class="site-footer__col-four">
-                <h3 class="headline headline--small">Kontakt</h3>
+            <div class="">
+                <h3 class="">Social media</h3>
                 <nav>
-                    <ul class="min-list social-icons-list group">
+                    <ul class="">
                         <li>
-                            <a href="#" class="social-color-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="#" class=""><i class="fa fa-facebook" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="social-color-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="#" class=""><i class="fa fa-twitter" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="social-color-youtube"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                            <a href="#" class=""><i class="fa fa-youtube" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="social-color-linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a href="#" class=""><i class="fa fa-linkedin" aria-hidden="true"></i></a>
                         </li>
                         <li>
-                            <a href="#" class="social-color-instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                            <a href="#" class=""><i class="fa fa-instagram" aria-hidden="true"></i></a>
                         </li>
                     </ul>
                 </nav>
