82e635b54d552b8943b4d591ff0645066bc8574c
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-06-06 22:10:20 +0200

    Remove classes for single post

--- single.php
+++ single.php
@@ -5,14 +5,14 @@ pageBanner();
 while(have_posts()) {
     the_post(); ?>
 
-    <div class="container container--narrow page-section">
-        <div class="metabox metabox--position-up metabox--with-home-link">
-            <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog</a> <span class="metabox__main"><?php the_author_posts_link(); ?>, <?php the_time('j F Y'); ?>, <?php echo get_the_category_list(', '); ?></span></p>
+    <div class="">
+        <div class="">
+            <p><a class="" href="<?php echo site_url('...'); ?>"><i class="fa fa-home" aria-hidden="true"></i> Blog</a> <span class=""><?php the_author_posts_link(); ?>, <?php the_time('j F Y'); ?>, <?php echo get_the_category_list(', '); ?></span></p>
         </div>
         <div>
             <?php the_post_thumbnail(); ?>
         </div>
-        <div class="generic-content"><?php the_content(); ?></div>
+        <div class=""><?php the_content(); ?></div>
 
     </div>
     <?php
